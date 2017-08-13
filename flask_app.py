7be2a034da612a62b335ca4c73460ad9a1d0a338
JonasSigge <jontesigge@gmail.com>
2017-08-13 14:53:38 +0200

    Project started

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,30 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, url_for, redirect, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Flask Boi!'
+def index():
+	return render_template('index.html')
 
+@app.route('/register')
+def register():
+	#TODO
+	return redirect(url_for('index'))
+
+@app.route('/login')
+def login():
+	#TODO
+	return redirect(url_for('index'))
+
+@app.route('/projectlist')
+def projectlist():
+	#TODO
+	return redirect(url_for('index'))
+
+@app.route('/articles')
+def articles():
+	#TODO
+	return redirect(url_for('index'))
