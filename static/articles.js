83b907591f696d6c062daf3e46865762be6d2239
unknown <jonas.gustafsson@MITAUTO,local>
2017-12-02 19:29:43 +0100

    Added search function for articles

---
+++ static/articles.js
@@ -0,0 +1,21 @@
+$(function()
+{
+
+  $("#searchbar").keyup(function() 
+  {
+    // Declare variables
+    var filter;
+    filter = $(this).val().toUpperCase();
+
+    $(".article_post").each(function(i, obj) 
+    {
+  
+      if ($("a",this).text().toUpperCase().indexOf(filter) > -1 )
+      {
+        $(this).css("display","");
+      }else{
+        $(this).css("display","none");
+      }
+    });
+  });
+});
